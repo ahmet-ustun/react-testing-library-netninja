b63a3ebe186870d922ef8a6acd159c9980296312
Ahmet Ustun <austu@softserveinc.com>
2023-04-04 15:40:06 +0300

    Use the query methods on the Header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,5 +2,10 @@ import React from "react";
 import "./Header.css";
 
 export default function Header({ title }) {
-  return <h1 className="header">{title}</h1>;
+  return (
+    <>
+      <h1 className="header" data-testid="header">{title}</h1>
+      <h3 className="header" title="Header">Cats</h3>
+    </>
+  );
 }
